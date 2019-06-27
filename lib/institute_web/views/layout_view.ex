b69141a069973646c362cf598262877e17a1a345
Kyle Oba <koba@mudphone.com>
2019-06-26 22:33:06 -1000

    Conditionally show Thesis menu

--- lib/institute_web/views/layout_view.ex
+++ lib/institute_web/views/layout_view.ex
@@ -1,3 +1,16 @@
 defmodule InstituteWeb.LayoutView do
   use InstituteWeb, :view
+
+  alias InstituteWeb.MeetingController
+  alias InstituteWeb.PageController
+
+  def show_thesis_editor?(conn) do
+    action = conn.private.phoenix_action
+    controller = conn.private.phoenix_controller
+    show_menu_on_page?({controller, action})
+  end
+
+  defp show_menu_on_page?({PageController, :index}), do: true
+  defp show_menu_on_page?({MeetingController, _action}), do: true
+  defp show_menu_on_page?({_, _}), do: false
 end
