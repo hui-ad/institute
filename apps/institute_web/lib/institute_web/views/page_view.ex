d029ecc9a673748365e6bbf5afc7a5a8a4cb2496
Kyle Oba <koba@mudphone.com>
2019-07-24 15:08:27 -1000

    Guard against bad DB column vals for page content

--- apps/institute_web/lib/institute_web/views/page_view.ex
+++ apps/institute_web/lib/institute_web/views/page_view.ex
@@ -14,4 +14,11 @@ defmodule InstituteWeb.PageView do
   def fmt_mtg_content(%{content: content, type: _type}) do
     content
   end
+
+  @doc """
+  Guards agains a nil stored in the DB.
+  
+  Returns blank as a last resort.
+  """
+  def fmt_mtg_content(_), do: ""
 end
