63ea5e62d72837faf3340ad597e179a77e54b01a
Jason Axelson <jason.axelson@gmail.com>
2019-08-03 22:36:24 -1000

    Only try to create the test db explicitly

--- apps/institute/mix.exs
+++ apps/institute/mix.exs
@@ -53,8 +53,7 @@ defmodule Institute.MixProject do
   defp aliases do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
     ]
   end
 end
